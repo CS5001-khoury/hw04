0ba6fbb963c76e50a1afdcb37aa5600206c75daa
Albert Lionelle <albert.lionelle@colostate.edu>
2023-01-10 11:55:47 -0700

    added template files

---
+++ number_magic.py
@@ -0,0 +1,12 @@
+"""
+Homework 4: Number Magic    
+=======================
+Course:   CS 5001
+Semester: UPDATE
+Student:  UPDATE
+
+
+A series of functions that deal with number manipulation and string concatenation 
+"""
+
+
---
+++ star_rating_app.py
@@ -0,0 +1,47 @@
+"""
+Homework 4: Star Rating App
+===========================
+Course:   CS 5001
+Semester: UPDATE
+Student:  UPDATE
+
+
+An application that queries the client for movie titles
+and a rating for each movie.
+"""
+
+
+_MIN_STARS = 1
+_MAX_STARS = 5
+
+
+def add_movie():
+    pass
+
+
+def get_movie():
+    pass
+
+
+def get_rating():
+    pass
+
+
+def get_valid_int(prompt):
+    pass
+
+def convert_rating(val):
+    pass
+
+def list_movies(movies):
+    pass
+
+def menu():
+    pass
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
